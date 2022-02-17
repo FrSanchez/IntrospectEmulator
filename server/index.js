f36eb6a9fbe16f9f241ee5cbbd89550923af6881
Francisco SancheZ <francisco.sanchez@salesforce.com>
2022-02-17 11:26:20 -0800

    Remove service files

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app");
-const log4js = require("log4js");
-const PORT = process.env.PORT || 8080
-const logger = log4js.getLogger();
-logger.level = 'debug';
-
-app.listen(PORT, () => {
-    logger.info(`Fake introspector app listening at port ${PORT}`)
-});
