f9628e48248d1a1607fe91619811bc81d18bd612
Francisco Sanchez <francisco.sanchez@salesforce.com>
2022-09-08 11:07:00 -0700

    Handle errors

--- src/index.js
+++ src/index.js
@@ -22,19 +22,16 @@ app.use((req, res, next) => {
 
   app.use('/services/oauth2', routes.oauth2);
   app.use('/api/tokens', routes.token);
+
   app.use(express.static(__dirname + '/static'));
 
-    //  app.use((error, req, res) => {
-//     if (!error.statusCode) error.statusCode = 500;
-  
-//     if (error.statusCode === 301) {
-//       return res.status(301).redirect('/not-found');
-//     }
-  
-//     return res
-//       .status(error.statusCode)
-//       .json({ error: error.toString() });
-//   });
+  app.get('*', function (req, res, next) {
+    res.status(301).redirect('/not-found');
+  });
+
+  app.use((error, req, res, next) => {
+    return res.status(500).json({ error: error.toString() });
+  });
 
   db.sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
     if (eraseDatabaseOnSync) {
